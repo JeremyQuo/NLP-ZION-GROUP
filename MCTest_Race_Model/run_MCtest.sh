74e43855b0bb0f6d8eef86b807aa06a21f1d5031
JessieHo96 <70374726+JessieHo96@users.noreply.github.com>
2021-04-26 23:31:01 +0800

    Create run_MCtest.sh

---
+++ MCTest_Race_Model/run_MCtest.sh
@@ -0,0 +1,14 @@
+python MCTest_train.py\
+ --data_dir=/data/MCTest/MCTest/ \
+ --bert_model=bert-base-uncased \
+ --output_dir=large_models \
+ --max_seq_length=256 \
+ --do_train \
+ --do_eval  \
+ --do_lower_case \
+ --train_batch_size=4 \
+ --eval_batch_size=4 \
+ --learning_rate=1e-5 \
+ --num_train_epochs=2 \
+ --gradient_accumulation_steps=1 \
+ --loss_scale=0
